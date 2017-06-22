aec44292bb662dc6f95b134dd0096f82e88d916b
Jan-Erik Revsbech <janerik@moc.net>
2017-06-22 11:17:32 +0200

    Working version that saves to output

--- detect.py
+++ detect.py
@@ -3,57 +3,76 @@ import sounddevice as sd
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 import numpy as np
-import wave
-wsize = 4096
-wratio = 0.5
+import pickle
+from scipy.ndimage.filters import maximum_filter
+from scipy.ndimage.morphology import (generate_binary_structure,
+                                      iterate_structure, binary_erosion)
+from scipy import signal
+from detect_peak import detect_peaks
+
+#The sice of the window usedn then doing the FFT
+wsize = 4092
+wratio = 0.0
 samplerate = 16000
+#The sample index
+index = 1
 
-signal, samplerate = sf.read('test.raw', channels=1, samplerate=16000,  format='RAW', subtype='PCM_16')
+filename = 'test.raw'
+outputFilename = 'dataoutput.b'
 
-
-Time=np.linspace(0, len(signal)/samplerate, num=len(signal))
-
-
-#plt.show()
-#sd.play(signal, samplerate)
-
-
-
-#Y = np.fft.fft(signal)
-#plt.plot(Time, Y)
-#plt.show()
+signalData, samplerate = sf.read(filename, channels=1, samplerate=samplerate,  format='RAW', subtype='PCM_16')
+print "Data length: ", len(signalData)
+print "Slitting timeseries into ", len(signalData) / wsize, " windows"
+numberOfWindows = len(signalData) / wsize
+#The timespan of the clip
+Time=np.linspace(0, len(signalData)/samplerate, num=len(signalData))
 
 
 
 ax1 = plt.subplot(311)
-plt.plot(Time,signal)
-#plt.subplot(312, sharex=ax1)
+ax1.set_xlabel('Time')
+ax1.set_ylabel('Amplitude')
+plt.plot(Time,signalData)
+
 plt.subplot(312)
+#plt.subplot(312, sharex=ax1)
+
 
 spectrum, freqs, t, im = plt.specgram(
-        signal,
+        signalData,
         NFFT=wsize,
-        Fs=4,
+        Fs=samplerate,
         window=mlab.window_hanning,
         noverlap=int(wsize * wratio))
 
 
-index = 17
 
-print len(spectrum)
+outArr = []
+for x in range(0, numberOfWindows):
+	sampleData = spectrum[...,x]
 
-plt.subplot(313)
+	#Take the log
+	#sampleData = 10 * np.log10(1000 * sampleData)
+
+	ax3 = plt.subplot(313)
+
+	#Find the peaks that are greater than twice the meanVAlue
+	meanValue = np.mean(sampleData)
+	max_peaks = detect_peaks(sampleData, mph=meanValue * 2)
+	peak_values = sampleData[max_peaks]
+
+	print "Snapshow taken after", t[x], " seconds"
+	plt.plot(sampleData)
+	plt.scatter(max_peaks, peak_values, s=30)
+
+	outdata = {'Sourcefilename': filename, 'window_start_position': t[x], 'window_number': x, 'peak_frequency': freqs[max_peaks], 'peak_value': peak_values}
+	outArr.append(outdata)
 
 
-#plt.plot(spectrum[index])
-#print spectrum[index]
-#print len(spectrum[index])
-#print len(t)
-#print len(freqs)
-#print len(spectrum[...,index])
-print "Snapshow taken after", t[index], " seconds"
-plt.plot(freqs,spectrum[...,index])
-plt.show()
+pickle.dump(outArr,  open( outputFilename, "wb" ))
 
+
+#print outArr
+#plt.show()
 
 
